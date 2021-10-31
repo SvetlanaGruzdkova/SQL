97a110326ecf0728ccd01f9589d1fa18b537f115
Светлана Груздкова <91968726+SvetlanaGruzdkova@users.noreply.github.com>
2021-10-31 14:16:18 +0200

    SQL_DDL_Create table_Kurs V.Ksendzov

--- SQL_HW2_Gruzdkova.sql
+++ SQL_HW2_Gruzdkova.sql
@@ -1,8 +1,9 @@
 
---Создать таблицу employees
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees
 --- id. serial,  primary key,
 --- employee_name. Varchar(50), not null
---Наполнить таблицу employee 70 строками.
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё.
+
 create table employees (
 id serial primary key,
 employee_name varchar (50) not null
@@ -11,10 +12,11 @@ insert into employees (id, employee_name)
 values (default, 'Natasha Zagoryanskaya');
 select * from employees;
 --====================================================
---Создать таблицу salary
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary
 --- id. Serial  primary key,
 --- monthly_salary. Int, not null
---Наполнить таблицу salary 15 строками:
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё:
+
 create table salary (
 id serial primary key,
 monthly_salary varchar (30) not null
@@ -26,12 +28,13 @@ insert into salary(id, monthly_salary)
 values (default, 2400);
 select * from salary;
 --=====================================================
---Создать таблицу employee_salary
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary
 --- id. Serial  primary key,
 --- employee_id. Int, not null, unique
 --- salary_id. Int, not null
---Наполнить таблицу employee_salary 40 строками:
---- в 10 строк из 40 вставить несуществующие employee_id
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё:
+-- РІ 10 СЃС‚СЂРѕРє РёР· 40 РІСЃС‚Р°РІРёС‚СЊ РЅРµСЃСѓС‰РµСЃС‚РІСѓСЋС‰РёРµ employee_id
+
 create table employee_salary (
 id serial  primary key,
 employee_id int not null unique,
@@ -47,11 +50,13 @@ insert into employee_salary (id, employee_id, salary_id)
 values (default, 79, 13);
 select * from employee_salary;
 --=======================================================
---Создать таблицу roles
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles
 --- id. Serial  primary key,
 --- role_name. int, not null, unique
---Поменять тип столба role_name с int на varchar(30)
---Наполнить таблицу roles 20 строками:
+--РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё:
+
+
 create table roles (
 id serial primary key,
 role_name int not null unique
@@ -63,11 +68,11 @@ insert into roles (id, role_name)
 values (default, 'Senior Automation QA engineer');
 select * from roles;
 --==========================================================
---Создать таблицу roles_employee
+--РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee
 --- id. Serial  primary key,
---- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
---- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
---Наполнить таблицу roles_employee 40 строками:
+--- employee_id. Int, not null, unique (ГўГ­ГҐГёГ­ГЁГ© ГЄГ«ГѕГ· Г¤Г«Гї ГІГ ГЎГ«ГЁГ¶Г» employees, ГЇГ®Г«ГҐ id)
+--- role_id. Int, not null (РІРЅРµС€РЅРёР№ РєР»СЋС‡ РґР»СЏ С‚Р°Р±Р»РёС†С‹ roles, РїРѕР»Рµ id)
+--РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё:
 
 create table roles_employee (
 id Serial  primary key,

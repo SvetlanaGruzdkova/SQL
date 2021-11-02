797b8cfaf035d75f548c187ee53e8f6407cecfe4
Светлана Груздкова <91968726+SvetlanaGruzdkova@users.noreply.github.com>
2021-11-02 20:19:24 +0200

    Update SQL_HW3_Join_Gruzdkova.sql

--- SQL_HW3_Join_Gruzdkova.sql
+++ SQL_HW3_Join_Gruzdkova.sql
@@ -1,4 +1,4 @@
---1. Âûâåñòè âñåõ ðàáîòíèêîâ ÷üè çàðïëàòû åñòü â áàçå, âìåñòå ñ çàðïëàòàìè.
+--1.  Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами.
 select employees.employee_name, salary.monthly_salary 
 from employees join employee_salary 
 on employee_salary.employee_id = employees.id
@@ -13,7 +13,7 @@ join salary
 on employee_salary.salary_id = salary.id
 where monthly_salary < 2000;
 
--- 3. Âûâåñòè âñå çàðïëàòíûå ïîçèöèè, íî ðàáîòíèê ïî íèì íå íàçíà÷åí. (ÇÏ åñòü, íî íå ïîíÿòíî êòî å¸ ïîëó÷àåò.)
+-- 3. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
 select salary.monthly_salary 
 from employees right join employee_salary 
 on employee_salary.employee_id = employees.id
@@ -21,8 +21,7 @@ right join salary
 on employee_salary.salary_id = salary.id
 where employee_name is null;
 
--- 4. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
-select salary.monthly_salary 
+-- 4. Вывести все зарплатные позиции  меньше 2000 но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
 from employees right join employee_salary 
 on employee_salary.employee_id = employees.id
 right join salary 
@@ -251,7 +250,7 @@ join roles
 on roles_employee.role_id = roles.id
 where role_name like '%QA%';
 
--- 26. Âûâåñòè êîëè÷åñòâî Middle ñïåöèàëèñòîâ.
+-- 26. Вывести количество Middle специалистов.
 select count (role_name)
 from roles_employee join employees 
 on roles_employee.employee_id = employees.id
@@ -259,7 +258,7 @@ join roles
 on roles_employee.role_id = roles.id
 where role_name like '%Middle%';
 
--- 27. Âûâåñòè êîëè÷åñòâî ðàçðàáîò÷èêîâ
+-- 27. Вывести количество разработчиков
 select count (role_name)
 from roles_employee join employees 
 on roles_employee.employee_id = employees.id
@@ -267,7 +266,7 @@ join roles
 on roles_employee.role_id = roles.id
 where role_name like '%developer';
 
--- 28. Âûâåñòè ôîíä (ñóììó) çàðïëàòû ðàçðàáîò÷èêîâ.
+-- 28. Вывести фонд (сумму) зарплаты разработчиков.
 select sum (salary.monthly_salary)  
 from salary join employee_salary  
 on salary.id = employee_salary.salary_id
@@ -279,7 +278,7 @@ join roles
 on  roles_employee.role_id = roles.id 
 where role_name like '%developer';
 
--- 29. Âûâåñòè èìåíà, äîëæíîñòè è ÇÏ âñåõ ñïåöèàëèñòîâ ïî âîçðàñòàíèþ
+-- 29. Вывести имена, должности и ЗП всех специалистов по возрастанию
 select employees.employee_name, salary.monthly_salary, roles.role_name 
 from employees join employee_salary
 on employee_salary.employee_id = employees.id
@@ -291,7 +290,7 @@ join roles
 on roles_employee.role_id = roles.id
 order by monthly_salary asc;
 
--- 30. Âûâåñòè èìåíà, äîëæíîñòè è ÇÏ âñåõ ñïåöèàëèñòîâ ïî âîçðàñòàíèþ ó ñïåöèàëèñòîâ ó êîòîðûõ ÇÏ îò 1700 äî 2300
+-- 30. Вывести имена, должности и ЗП всех специалистов по возрастанию у специалистов у которых ЗП от 1700 до 2300
 select employees.employee_name, salary.monthly_salary, roles.role_name 
 from employees join employee_salary
 on employee_salary.employee_id = employees.id
@@ -304,7 +303,7 @@ on roles_employee.role_id = roles.id
 where monthly_salary between 1700 and 2300
 order by monthly_salary asc;
 
--- 31. Âûâåñòè èìåíà, äîëæíîñòè è ÇÏ âñåõ ñïåöèàëèñòîâ ïî âîçðàñòàíèþ ó ñïåöèàëèñòîâ ó êîòîðûõ ÇÏ ìåíüøå 2300
+-- 31. Вывести имена, должности и ЗП всех специалистов по возрастанию у специалистов у которых ЗП меньше 2300
 select employees.employee_name, salary.monthly_salary, roles.role_name 
 from employees join employee_salary
 on employee_salary.employee_id = employees.id
@@ -317,7 +316,7 @@ on roles_employee.role_id = roles.id
 where monthly_salary < 2300
 order by monthly_salary asc;
 
--- 32. Âûâåñòè èìåíà, äîëæíîñòè è ÇÏ âñåõ ñïåöèàëèñòîâ ïî âîçðàñòàíèþ ó ñïåöèàëèñòîâ ó êîòîðûõ ÇÏ ðàâíà 1100, 1500, 2000
+-- 32. Вывести имена, должности и ЗП всех специалистов по возрастанию у специалистов у которых ЗП равна 1100, 1500, 2000
 select employees.employee_name, salary.monthly_salary, roles.role_name 
 from employees join employee_salary
 on employee_salary.employee_id = employees.id
